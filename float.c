5b0e86ad0c7fcece93546c30f6cdee3dbcdc7b4c
kartoone <brtoone@samford.edu>
2021-10-16 09:16:19 -0500

    midterm review updates

--- float.c
+++ float.c
@@ -2,15 +2,21 @@
 #include <stdlib.h>
 
 int main() {
-  float f1 = 3.75f;
-  float f2 = 0b01000000011100000000000000000000;  // 11.11
-  float f3 = 0x40700000;
-  float f4 = 0x00007040;
-                       // 1.111 x 2^1
-                       // 128
-  printf("%f\n", f1);
-  printf("%f\n", f2);
-  printf("%f\n", f3);
-  printf("%f\n", f4);
-  return 0;
+	// f1 is the normal way a floating point number is initialized ... and C takes care of converting it into the IEEE754 representation
+	float f1 = 3.75f;
+	
+	// f2 is being set to an integer that represents the bits of the IEEE754 encoded floating point number 3.75
+	int f2 = 0b01000000011100000000000000000000;  // Step 1: 11.11 
+	                     							// Step 2: 1.111 x 2^1
+	                     							// Step 3: explonent = 1 + 127 = 128
+	
+	// this is just the HEX representation of the bit pattern used for f2
+	int f3 = 0x40700000; // NOTE that f2 and f3 are BOTH going to be stored in little endian order (0x00004070) so we don't need to manually reverse the bytes
+	
+	// you will see that these printf calls below all print out the same thing
+	// we have to do the pointer trickery to keep C from converting the integer automatically to the corresponding floating point number
+	printf("%f\n", f1);
+	printf("%f\n", *((float*) &f2));
+	printf("%f\n", *((float*) &f3));
+	return 0;
 }
--- overflow.c
+++ overflow.c
@@ -3,34 +3,34 @@
 int main() {
   //unsigned char num1 = 0b11111111; // maximum positive char
   unsigned char num1 = 255;
-  printf("%d\n",num1); // no overflow 
+  printf("num1: %d\n",num1); // no overflow 
   
   char num1_overflow = 255;
-  printf("%d\n",num1_overflow); // we can't represent postive 255 using a single signed byte. C interprets as a two's complement byte, so it displays -1 
+  printf("num1_overflow: %d\n",num1_overflow); // we can't represent postive 255 using a single signed byte. C interprets as a two's complement byte, so it displays -1 
 
   unsigned char num2 = 0b1;
   unsigned char result1_overflow = num1 + num2; // when we add 1 to 255, we get 256, which is 0b100000000 (NINE bits, the extra 1 is thrown away yielding 0 for the result)
-  printf("%d + %d = %d\n", num1, num2, result1_overflow);
+  printf("result1_overflow: %d + %d = %d\n", num1, num2, result1_overflow);
 
   char num3 = 127;                      // largest possible POSITIVE integer using 8 bits 0b01111111
   char num4 = 1;                        // doesn't take much to overflow since it was already the largest positive integer!!!
   char result2_overflow = num3 + num4;  // 127 + 1 = 128 which in binary is a 0b10000000, which is a NEGATIVE number in two's complement ... so overflow has happened!
-  printf("%d + %d = %d\n", num3, num4, result2);
+  printf("result2_overflow: %d + %d = %d\n", num3, num4, result2_overflow);
 
   // same as earlier examples but using 4 bytes (int) instead of 1 byte (char)
   unsigned int i1 = 0xffffffff;
-  printf("%d\n", i1);
+  printf("i1: %u\n", i1);
   
   int i1_overflow = 0xffffffff;
-  prtinf("%d\n", i1_overflow);
+  printf("i1_overflow: %d\n", i1_overflow);
   
   unsigned int i2 = 1;
   unsigned int resulti1_overflow = i1 + i2;
-  printf("%d + %d = %d\n", i1, i2, resulti1_overflow);
+  printf("resulti1_overflow: %u + %u = %u\n", i1, i2, resulti1_overflow);
 
   int i3 = 0x7fffffff; // 0b01111111111111111111111111111111
   int i4 = 1;
   int resulti2_overflow = i3 + i4;  // 2147483648 which is equal to 0b10000000000000000000000000000000 (which is NEGATIVE 2147483648)
-  printf("%d + %d = %d\n", i3, i4, resulti2_overflow); // overflow has happened b/c we added two positive numbers and got a negative
+  printf("resulti2_overflow: %d + %d = %d\n", i3, i4, resulti2_overflow); // overflow has happened b/c we added two positive numbers and got a negative
   return 0;
 }
