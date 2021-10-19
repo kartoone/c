74011ff15b003f0f78382ea5b8237f1d39d2d157
Brian Toone <brtoone@samford.edu>
2021-10-19 09:43:39 -0500

    example from chapter 6 plus pointer demo

--- example.c
+++ example.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int adder() {
     int a;
@@ -12,8 +13,12 @@ int assign() {
 
 int main(int argc, char* argv[]) {
     int x;
+    int* y;
+    y = malloc(sizeof(y));
+    *y = 6;
     assign();
     x = adder();
-    printf("x is: %d\n", x);
+    printf("x stored at %p has a value of : %d\n", &x, x);
+    printf("y stored at %p has a value of : %d\n", y, *y);
     return 0;
 }
