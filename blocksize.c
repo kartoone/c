942a4e7fa0b01c48e3166c4cc83d953d30c63bb5
Brian Toone <brtoone@samford.edu>
2021-12-09 10:31:42 -0600

    parallelization examples

--- blocksize.c
+++ blocksize.c
@@ -16,6 +16,12 @@ int *genRandomMatrix(int n, int max) {
     return mat;
 }
 
+void flushcache(int size) {
+	char *c = (char*)malloc(size);
+	for (int i=0; i<size; i++)
+		c[i] = i%256;
+}
+
 // spacing - the matrix offset to read a given number of bytes away
 void consecutiveReads(int* matrix, int spacing) {
 	printf("Part 1: block size determinination\n");
@@ -26,21 +32,45 @@ void consecutiveReads(int* matrix, int spacing) {
 // sa - the known set associativity
 void associativityTest(int * matrix, int spacing, int sa) {
 	printf("Part 2: associativity verification\n");
-	printf("These should generate %d misses b/c they are ALL misses.\n", sa);
-	for (int i=0; i<sa; i++) {
-		printf("matrix[%d]: %d\n",i, matrix[i*spacing]);
+	printf("Total cache amount: 32768 bytes\n");
+	printf("Each cache line is 64 bytes\n");
+	printf("But each cache line is 8-way set associative, which means there there are 64*8=%d bytes in each set\n", 64*8);
+	printf("Total number of cache lines then is: 32768/512 = %d\n", 32768/512);
+	printf("The same colored cache lines (i.e., the same cache line) are  = %d bytes offset from each other\n", 64*64);
+	printf("In terms of our matrix where each entry takes up 4 bytes, %d bytes is equivalent to %d/4=%d indices apart from each other\n", 64*64, 64*64, 64*64/4);
+	printf("This should be a miss: matrix[%d]: %d\n", 0, matrix[0]);
+	printf("Total misses SO FAR should be %d\n", 1);
+	printf("These should ALL be hits\n");
+	for (int i=0; i<16; i++) {
+		printf("matrix[%d]: %d\n", i, matrix[i]);
 	}
-	printf("These should generate %d hits b/c they are ALL hits.\n", sa);
-	for (int i=0; i<sa; i++) {
-		printf("matrix[%d+1]: %d\n",i+1, matrix[i*spacing+1]);
-	}
-	printf("These should generate %d misses b/c they are for the sets immediately after the ones currently loaded.\n", sa);
-	for (int i=sa; i<2*sa; i++) {
-		printf("matrix[%d]: %d\n",i, matrix[i*spacing]);
-	}
-	printf("Total misses should be %d + %d = %d\n", sa, sa, sa+sa);
-}
+	printf("Total misses should STIL be %d\n", 1);
 
+	// let's fill up the next SA block ... we've already filled 1
+	printf("This should be a miss: matrix[%d]: %d\n", 1024, matrix[1024]);
+	for (int i=0; i<16; i++) {
+		printf("This should be a hit: matrix[1024+%d]: %d\n", 1024+i, matrix[1024+i]);
+	}
+	printf("Total misses SO FAR should be %d\n", 2);
+	
+	// now let's fill up all SA blocks ... we've already filled 2
+	for (int si=2; si<8; si++) {
+		printf("This should be a miss: matrix[1024*%d]: %d\n", si, matrix[1024*si]);
+	}
+	printf("Total misses SO FAR should be %d\n", 8);
+	for (int si=1; si<8; si++) {
+		printf("This should be a hit: matrix[1024*%d]: %d\n", si, matrix[1024*si]);
+	}
+	printf("Total misses SO FAR should be %d\n", 8);
+
+	// now let's verify that we can read from ANY of those salmon colored blocks (cache block line 0)
+//	printf("These should ALL be hits still, but the 8 sets are all completely full for cache line 0\n");
+//	for (int i=0; i<8; i++) {
+//		printf("This should be a hit: matrix[%d*1024]: %d\n", i, matrix[i]);
+//	}
+//	printf("Total misses SO FAR should STILL be %d\n", 8);
+
+}
 
 int main(int argc, char **argv) {
     int i, n, bs, sa;
@@ -61,8 +91,9 @@ int main(int argc, char **argv) {
     srand(time(NULL));
     matrix = genRandomMatrix(n, 100);
 
-	consecutiveReads(matrix, bs/4); // divide by 4 b/c the matrix entries are each taking up 4 bytes 
-//	associativityTest(matrix, bs/4, sa); // divide by 4 b/c the matrix entries are each taking up 4 bytes 
+//	consecutiveReads(matrix, bs/4); // divide by 4 b/c the matrix entries are each taking up 4 bytes 
+	flushcache(32*1024);
+	associativityTest(matrix, bs/4, sa); // divide by 4 b/c the matrix entries are each taking up 4 bytes 
 
     free(matrix);
     return 0;
---
+++ matrixsumt.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <time.h>
+
+long *array; //allocated in main
+long length; //set in main (1 billion)
+long nthreads; //number of threads
+long s; //scalar
+
+void printArray() {
+	printf("columns represent different threads ...\n");
+    printf("displaying first 10 values of the matrix for each \"chunk\"\n");
+    for (int i = 0; i < 10; i++) {
+		 for (int t=0; t<nthreads; t++) {
+	   		int index = (length/nthreads)*t+i;
+	   		printf("%ld ", array[index]);
+       	 } 
+       	 printf("\n");
+    }
+}
+
+/*error handling function: prints out error message*/
+int print_error(char *msg) {
+    fprintf(stderr, "%s\n", msg);
+    exit(2);
+}
+
+void *matrix_sum(void *id) {
+    long *myid = (long *) id;
+    int i;
+	long *mysum = malloc(sizeof(long));
+
+	// init sum for this thread to 0
+	*mysum = 0;
+
+    //assign each thread its own chunk of elements to process
+    long chunk = length / nthreads;
+    long start = *myid * chunk;
+    long end  = start + chunk;
+    if (*myid == nthreads - 1) {
+        end = length;
+    }
+
+    //perform scalar multiplication on assigned chunk
+    for (i = start; i < end; i++) {
+        *mysum += array[i];
+    }
+
+    return mysum;
+}
+
+int main(int argc, char **argv) {
+    int i;
+    pthread_t *thread_array; //pointer to future thread array
+    long *thread_ids;
+    int ret; //useful for error detection
+
+    /* Read the number of threads to create from the command line. */
+    if (argc != 4) {
+        fprintf(stderr, "usage: %s int int int<n>\n", argv[0]);
+        return 1;
+    }
+    nthreads = strtol(argv[1], NULL, 10); //get number of threads
+    length = strtol(argv[2], NULL, 10); //get length of array
+    s = strtol( argv[3], NULL, 10 ); //get scaling factor
+	printf("ALLOCATING ARRAY...\n");
+    array = malloc(length*sizeof(long));
+    for (i=0; i<length; i++) {
+      array[i] = (long)i;
+    }
+    printf("STARTING ADDITION: \n");
+
+    /* Allocate space for thread structs and identifiers. */
+	struct timespec start, finish;
+	double elapsed;
+  	clock_gettime(CLOCK_REALTIME, &start);
+    thread_array = malloc(nthreads * sizeof(pthread_t));
+    thread_ids = malloc(nthreads * sizeof(long));
+    if (!thread_array || !thread_ids) print_error("ERROR: malloc failed");
+
+    /* Assign each thread an ID and create all the threads. */
+    for (i = 0; i < nthreads; i++) {
+        thread_ids[i] = i;
+        ret = pthread_create(&thread_array[i], NULL, matrix_sum, &thread_ids[i]);
+        if (ret) print_error("ERROR: pthread create failed");
+    }
+
+    /* Join all the threads. Main will pause in this loop until all threads
+     * have returned from the thread function. */
+	long grand_total = 0;
+    for (i = 0; i < nthreads; i++) {
+		void *retval;
+        ret = pthread_join(thread_array[i], &retval);
+		grand_total += *(long*)retval;
+        if (ret) print_error("ERROR: pthread join failed");
+		free(retval);
+    }
+  	clock_gettime(CLOCK_REALTIME, &finish);
+	elapsed = (finish.tv_sec - start.tv_sec);
+	elapsed += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
+
+    printf("Summation result: %ld, %ld threads took %f s total\n", grand_total, nthreads, elapsed);
+
+    free(thread_array);
+    free(thread_ids);
+
+    return 0;
+}
+
